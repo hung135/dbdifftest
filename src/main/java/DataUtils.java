40ec5c1d28d6a9422ba31b0fffdb7ef623a89058
Hung Nguyen <Hung135@hotmail.com>
2019-09-14 18:26:41 +0000

    regext to list

--- src/main/java/DataUtils.java
+++ src/main/java/DataUtils.java
@@ -1,4 +1,5 @@
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,18 +19,18 @@ public class DataUtils {
      * @param regExString
      * @param dataString
      */
-    public static Collection<String> RegexExtract(String regExString, String dataString) {
-        System.out.println("xxxx");
-        System.out.println("xxxxxxx");
-        Collection<String> xx = null;
+    public static List<String> RegexExtract(String regExString, String dataString) {
+        List<String> xx = new ArrayList<String>();
 
-        String mydata = "some string with 'the data i want' inside";
-        Pattern pattern = Pattern.compile("'(.*?)'");
-        Matcher matcher = pattern.matcher(mydata);
-        if (matcher.find()) {
-            System.out.println(matcher.group(1));
+        Pattern pattern = Pattern.compile(regExString);
+        Matcher matcher = pattern.matcher(dataString);
+        while (matcher.find()) {
+            // System.out.println(matcher.group());
+            xx.add(matcher.group());
         }
+
         return xx;
 
     }
+
 }
--- src/test/java/DataUtilsTest.java
+++ src/test/java/DataUtilsTest.java
@@ -3,7 +3,9 @@ import junit.framework.TestCase;
 
 public class DataUtilsTest extends TestCase {
 
-    public void testLucky() {
-        assertEquals(7, DataUtils.RegexExtract("aaa", " 'a' 'b' 'c'"));
+    public void testFunctions() {
+
+        System.out
+                .println(DataUtils.RegexExtract("\\w*.\\.\\.", " from db1..table1, db2..table2, db3..table3 'b' 'c'"));
     }
 }

ed87a0b79e2f18b3b3445a19b67a1bff2eb3372e
Hung Nguyen <Hung135@hotmail.com>
2019-09-14 23:37:14 +0000

    parse table from query

--- src/main/java/DataUtils.java
+++ src/main/java/DataUtils.java
@@ -1,7 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.List;
+import java.util.Arrays;
 
 /**
  * Functions we need to write that process some data, put them here
@@ -12,6 +14,19 @@ public class DataUtils {
         System.out.println("Constructor called");
     }
 
+    // Generic function to convert list to set
+    public static <T> Set<T> convertListToSet(List<T> list) {
+        // create an empty set
+        Set<T> set = new HashSet<>();
+
+        // Add each element of list into the set
+        for (T t : list)
+            set.add(t);
+
+        // return the set
+        return set;
+    }
+
     /**
      * Take a regex and a data string and extract all the data that matches the
      * regex and returns it as a collection of string
@@ -19,8 +34,8 @@ public class DataUtils {
      * @param regExString
      * @param dataString
      */
-    public static List<String> RegexExtract(String regExString, String dataString) {
-        List<String> xx = new ArrayList<String>();
+    public static Set<String> RegexExtract(String regExString, String dataString) {
+        Set<String> xx = new HashSet<String>();
 
         Pattern pattern = Pattern.compile(regExString);
         Matcher matcher = pattern.matcher(dataString);
@@ -33,4 +48,49 @@ public class DataUtils {
 
     }
 
+    public static Set<String> FindSybaseDatabase(String dataString) {
+        Set<String> xx = new HashSet<String>();
+
+        Pattern pattern = Pattern.compile("\\w*\\.\\.");
+        Matcher matcher = pattern.matcher(dataString);
+        while (matcher.find()) {
+            // System.out.println(matcher.group());
+            xx.add(matcher.group().replace(".", ""));
+        }
+
+        return xx;
+
+    }
+
+    public static Set<String> findTablesFromInsert(String dataString) {
+
+        String x = dataString;
+
+        x = x.replaceAll("\n", " ");
+        x = x.replaceAll("(?i)insert (?i)into ", "~~");
+        System.out.println(x);
+        List<String> items = Arrays.asList(x.split("~~"));
+        return convertListToSet(items);
+    }
+
+    public static Set<String> findTablesFromQuery(String dataString) {
+
+        String x = dataString;
+        x = x.replaceAll("\n", " ");
+        x = x.replaceAll("^.*? select ", " select ");
+        x = x.replaceAll("(?i)select .*? from ", "~~");
+        x = x.replaceAll(" from ", "~~");
+        x = x.replaceAll(" join ", "~~");
+        x = x.replaceAll("(?i)where .*?\\(", "~~");
+        x = x.replaceAll("\\).*?\\(", "~~");
+        x = x.replaceAll("~~.*?(?i) from ", "~~");
+        x = x.replaceAll("(?i)select .*? from ", "~~");
+        x = x.replaceAll(" ", "");
+        x = x.replaceAll("\\)~", "~");
+        x = x.replaceAll("\\).*$", "~~");
+        x = x.replaceAll("~~~~", "~~");
+        List<String> items = Arrays.asList(x.split("~~"));
+        return convertListToSet(items);
+
+    }
 }
--- src/test/java/DataUtilsTest.java
+++ src/test/java/DataUtilsTest.java
@@ -7,5 +7,9 @@ public class DataUtilsTest extends TestCase {
 
         System.out
                 .println(DataUtils.RegexExtract("\\w*.\\.\\.", " from db1..table1, db2..table2, db3..table3 'b' 'c'"));
+        System.out.println(DataUtils.FindSybaseDatabase(
+                " from db1..table1, db2..table2, db3..table3, db3.dbo.table4  where something = something '"));
+        System.out.println(DataUtils.findTablesFromQuery(
+                " dsfasdfa tdhei screasdf; asdfasdf before select select col1, (select 1 from xxx.xx) as z, aa,(select 1 from xxx.xx) from schema.table join x.table2   where x=(select abc from test.table1) as y "));
     }
 }

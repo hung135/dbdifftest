5aae39a911586faba898a121af5d316536042998
deckerc <christian.decker@cfpb.gov>
2019-09-18 18:57:50 +0000

    Added arguements to dbdiff

--- jython_scripts/dbdiff.py
+++ jython_scripts/dbdiff.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 #JAVA ITEMS
 sys.path.append("/workspace/target/DbTest-jar-with-dependencies.jar")
@@ -14,9 +15,9 @@ from objects.database import Database
 SELECTQUERY = """SELECT c.text FROM sysusers u, syscomments c, sysobjects o 
                 WHERE o.type = 'P' AND o.id = c.id AND o.uid = u.uid  ORDER BY o.id, c.colid""";
 
-def readyaml():
+def readyaml(yaml):
     parser = YamlParser() # NOT THREAD SAFE
-    parsed_yaml = parser.readyaml()
+    parsed_yaml = parser.readyaml(yaml)
 
     database_objects, tasks = [], []
     for key in parsed_yaml:
@@ -45,6 +46,16 @@ def execute_sql_test_sysbase(database_objects):
             result = db.queryToList(SELECTQUERY)
             print(result)
 
+def parse_cli():
+    parser = argparse.ArgumentParser(description='Process a yaml file')
+    parser.add_argument("-y", help="Location of the yaml file")
+    args = parser.parse_args()
+    return args 
+
+def execute(args):
+    databases = readyaml(args.yaml)
+    print(databases)
 
 if __name__ == "__main__":
-    readyaml()
+    args = parse_cli()
+    execute(args)

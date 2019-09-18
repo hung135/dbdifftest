0ae336a77b241d1224affce89bd9c0bfa82dbd17
deckerc <christian.decker@cfpb.gov>
2019-09-18 13:44:34 +0000

    Created an sql executor example

--- jython_scripts/objects/database.py
+++ jython_scripts/objects/database.py
@@ -1,3 +1,5 @@
+import os
+
 class Database(object):
     dbtype = None
     host = None
@@ -5,12 +7,17 @@ class Database(object):
     user = None
     password_envar = None
     database_name = None
+    password = None
 
     def __init__(self, hashMap):
         self._set_values(hashMap)
 
     def _set_values(self, hashMap):
         for entry in hashMap.entrySet():
+            if entry.key == "password_envar":
+                setattr(self, entry.key, entry.value)
+                setattr(self, "password", os.getenv(entry.value))
+
             setattr(self, entry.key, entry.value)
 
     def __repr__(self):
--- jython_scripts/yaml.py
+++ jython_scripts/yaml.py
@@ -1,12 +1,18 @@
 import sys
+
+#JAVA ITEMS
 sys.path.append("/workspace/target/DbTest-jar-with-dependencies.jar")
 import YamlParser
+import DbConn
+import DataUtils
 
 from objects.database import Database
 
 # Notes:
 # jython is python 2.7.1 
 # It doesn't support list comp
+SELECTQUERY = """SELECT c.text FROM sysusers u, syscomments c, sysobjects o 
+                WHERE o.type = 'P' AND o.id = c.id AND o.uid = u.uid  ORDER BY o.id, c.colid""";
 
 def readyaml():
     parser = YamlParser() # NOT THREAD SAFE
@@ -26,6 +32,18 @@ def readyaml():
     print("\nTasks found:")
     for task in tasks:
        print("\t{0}".format(task))
+
+    print("Executing sysbase")
+    execute_sql_test_sysbase(database_objects)
+
+
+def execute_sql_test_sysbase(database_objects):
+    for base in database_objects:
+        if base.dbtype == "SYBASE":
+            sysBaseConnection = DbConn.DbType.SYBASE
+            db = DbConn(sysBaseConnection, base.user, base.password, base.host, base.port, base.database_name);
+            result = db.queryToList(SELECTQUERY)
+            print(result)
 
 
 if __name__ == "__main__":

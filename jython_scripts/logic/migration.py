dda004e1633cdda8a9791df6c74e309824632a66
Hung Nguyen <Hung135@hotmail.com>
2019-09-19 23:40:40 +0000

    fix logic

--- jython_scripts/logic/migration.py
+++ jython_scripts/logic/migration.py
@@ -13,10 +13,12 @@ class TableDump(object):
         path=os.path.abspath(writePath)
         if not os.path.exists(path):
             os.makedirs(path)
+        
         print("All the tables in this DB")
         x=(dbConn.getTableNames(schemaOrOwner))
         for a in x:
-            dbConn.queryToCSV('select * from {0}.{1}'.format(schemaOrOwner,a),path+a+'.csv')
+            fqn=os.path.join(path,a+'.csv')
+            dbConn.queryToCSV('select * from {0}.{1}'.format(schemaOrOwner,a),fqn)
 
     def __repr__(self):
         return str(self.__dict__)
@@ -30,18 +32,18 @@ class TableRowCount(object):
         for a in x:
             tmp=[]
             tmp.append(a)
+            zz=[]
+            try:
+                zz = dbConn.queryToList('select count(*) from {0}.{1}'.format(schemaOrOwner,a))
+                for b in zz:
+                    for c in b:
+                        tmp.append(c)
+                tableCount.append(tmp)
+            except Exception, e:
+                tableCount.append([a,'SQL Execute ERROR'])
             
-            zz = dbConn.queryToList('select count(*) from {0}.{1}'.format(schemaOrOwner,a))
-            for b in zz:
-                for c in b:
-                    tmp.append(c)
-            
-            tableCount.append(tmp)
-      
-
         dataUtil= DataUtils()
 
-  
         print("writing table count: ",path)
         header=["TableName","RowCount"]
         outPutTable = csv.writer(open(path, 'w'), delimiter=',',
@@ -58,32 +60,42 @@ class TableRowCount(object):
 
 class TableSampleCheckSum(object):
     def __init__(self, dbConn,schemaOrOwner,writePath,sampleSize):
-    
-        path=os.path.abspath(writePath)
+        directory=os.path.dirname(writePath)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+         
         print("All the tables in this DB")
         x=(dbConn.getTableNames(schemaOrOwner))
         table_row_hash = []
         sql='select * from {0}.{1} limit {2}'
-        if dbConn.dbType=='SYBASE':
+        
+        if str(dbConn.dbType)=='SYBASE':
             sql='select TOP {2} * from {0}.{1}'
         
         for a in x:
-            sample= dbConn.queryToList(sql.format(schemaOrOwner,a),path+a+'.csv',sampleSize)
-            m = md5.new()
-            for row in sample:
+             
+            try:
+                sample= dbConn.queryToList(sql.format(schemaOrOwner,a, sampleSize))
+            
+                m = md5.new()
+                if len(sample)==0:
+                    table_row_hash.append([a,"Zero Rows"])
+                for row in sample:
+                    for col in row:
+                        #adds each column to md5 hash algorith,
+                        m.update(col)
+                    table_row_hash.append([a,m.hexdigest()])
+                #print("writing table sample Hash: ",writePath)
+                header=["TableName","SampleDataHash"]
                 
-                for col in row:
-    
-                        
-                    m.update(col)
-                table_row_hash.append([a,m.digest()])
-            print("writing table sample Hash: ",path)
-            header=["TableName","SampleDataHash"]
-            outPutTable = csv.writer(open(path, 'w'), delimiter=',',
-                                quotechar='|')
-            outPutTable.writerow(header)
-            for row in table_row_hash:
-                outPutTable.writerow(row)
+                outPutTable = csv.writer(open(writePath, 'w'), delimiter=',',
+                                    quotechar='|')
+                outPutTable.writerow(header)
+                for row in table_row_hash:
+                    outPutTable.writerow(row)
+            except Exception, e:
+                table_row_hash.append([a,'SQL Execute ERROR'])
+                 
 
     def __repr__(self):
         return str(self.__dict__)

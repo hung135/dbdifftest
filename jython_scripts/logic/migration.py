167bf5d03427e3d35bf0814aac73bc06bb94c901
deckerc <christian.decker@cfpb.gov>
2019-09-19 19:04:46 +0000

    Have new tasks executing

--- jython_scripts/logic/migration.py
+++ jython_scripts/logic/migration.py
@@ -56,27 +56,27 @@ class TableRowCount(object):
 
 class TableSampleCheckSum(object):
     def __init__(self, dbConn,schemaOrOwner,writePath,sampleSize):
-    path=os.path.abspath(writePath)
-    print("All the tables in this DB")
-    x=(dbConn.getTableNames(schemaOrOwner))
-    table_row_hash = []
+        path=os.path.abspath(writePath)
+        print("All the tables in this DB")
+        x=(dbConn.getTableNames(schemaOrOwner))
+        table_row_hash = []
 
-    for a in x:
-        sample= dbConn.queryToList('select * from {0}.{1} limit {2}'.format(schemaOrOwner,a),path+a+'.csv',sampleSize)
-        m = md5.new()
-        for row in sample:
-            
-            for col in row:
+        for a in x:
+            sample= dbConn.queryToList('select * from {0}.{1} limit {2}'.format(schemaOrOwner,a),path+a+'.csv',sampleSize)
+            m = md5.new()
+            for row in sample:
                 
-                m.update(col)
-        table_row_hash.append([a,m.digest()])
-    print("writing table sample Hash: ",path)
-    header=["TableName","SampleDataHash"]
-    outPutTable = csv.writer(open(path, 'w'), delimiter=',',
-                        quotechar='|')
-    outPutTable.writerow(header)
-    for row in table_row_hash:
-        outPutTable.writerow(row)
+                for col in row:
+                    
+                    m.update(col)
+            table_row_hash.append([a,m.digest()])
+        print("writing table sample Hash: ",path)
+        header=["TableName","SampleDataHash"]
+        outPutTable = csv.writer(open(path, 'w'), delimiter=',',
+                            quotechar='|')
+        outPutTable.writerow(header)
+        for row in table_row_hash:
+            outPutTable.writerow(row)
 
     def __repr__(self):
         return str(self.__dict__)
